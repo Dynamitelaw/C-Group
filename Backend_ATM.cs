647f574e2c55ac0735449a5c80cba761ce69a86a
juliasheth <30651228+juliasheth@users.noreply.github.com>
2018-02-23 17:27:00 -0500

    Add files via upload
    Added update file and restore file methods to save bank account information

--- Backend_ATM.cs
+++ Backend_ATM.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace ATM
 {
+    //Francesca and Julia wrote this class
     public class Account
     {
         public string name;
         public static int id = 0;
-        public short pin;
+        public int pin;
         public decimal balance;
 
-        public Account(string n, short p)
+        public Account(string n, int p)
         {
             name = n;
             pin = p;
@@ -18,23 +20,29 @@ namespace ATM
             id++;
         }
 
+        public Account(string n, int p, decimal b, int i)
+        {
+            name = n;
+            pin = p;
+            balance = b;
+            id = i;
+        }
+
         public void deposit(decimal amount)
         {
             balance += amount;
         }
 
-        public string withdraw(decimal amount)
+        public Boolean withdraw(decimal amount)
         {
             if ((balance - amount) < 0)
             {
-                string message = "Withdraw declined. Not enough money in balance.";
-                return message;
+                return false;
             }
             else
             {
                 balance = balance - amount;
-                string message = "Amount withdrawn.";
-                return message;
+                return true;
             }
         }
 
@@ -48,11 +56,6 @@ namespace ATM
             return id;
         }
 
-        public short get_pin()
-        {
-            return pin;
-        }
-
         public Boolean verify_pin(int inputted_pin)
         {
             if (inputted_pin == pin)
@@ -60,8 +63,15 @@ namespace ATM
             else
                 return false;
         }
+
+        public string toString()
+        {
+            string account_string = $"{name}\t{pin.ToString()}\t{id.ToString()}\t{balance.ToString()}";
+            return account_string;
+        }
     }
 
+    //Francesca and Julia wrote this class
     public class Bank
     {
         Dictionary<int,Account> accounts = new Dictionary<int,Account>();
@@ -74,6 +84,44 @@ namespace ATM
         public Account get_account_by_id(int x)
         {
             return accounts[x];
+        }
+
+        public void update_file()
+        {
+            StreamWriter sw = new StreamWriter("BankRecords.txt",false);
+            foreach(KeyValuePair<int,Account> entry in accounts)
+            {
+                sw.WriteLine(entry.Value.toString());
+            }
+            sw.Close(); 
+        }
+
+        public void restore_file()
+        {
+            StreamReader sr = new StreamReader("BankRecords.txt");
+            string[] lines = System.IO.File.ReadAllLines("BankRecords.txt");
+            foreach (string line in lines)
+            {
+                int i = 0;
+                int j = 0;
+                while (line[i] != "\t") {}
+                string name = line[0:i];
+                i++;
+                j = i;
+                while (line[i] != "\t") {}
+                int pin = Convert.ToInt32(line[j: i]);
+                i++;
+                j = i;
+                while (line[i] != "\t") {}
+                int id = Convert.ToInt32(line[j: i]);
+                i++;
+                j = i;
+                while (line[i] != "\t") {}
+                decimal balance = Convert.ToDecimal(line[j: i]);
+
+                Account new_account = Account(name, pin, balance, id);
+                accounts[new_account.get_id()] = new_account;
+            }
         }
     }
 
